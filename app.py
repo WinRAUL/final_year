e264640a3ae297a553095fbe1fefa62e51321565
WinRAUL <rahul.chamoli.12@gmail.com>
2023-02-07 14:56:08 +0530

    Home page upgrade

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from putComplain import putComplain
+from putComplain import putComplain, verifyLogin
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index.html')
+@app.route('/mainHome')                    #homePage
 def index():
-    return render_template('index.html')
+    return render_template('/mainHome.html')
 
-@app.route('/',methods=['POST'] )
+@app.route('/signup')                      #signup.html
+def signup():
+    return render_template('/signup.html')      
+
+@app.route('/login')                       #login.html == mainHome.html
+def login():
+    return render_template('/mainHome.html')
+
+@app.route('/verify', methods=['POST'])                       #login verify and redirection
+def verify():
+    if verifyLogin(request.form['logemail'], request.form['pswd']):
+        print("idhar aagya hai")
+        return render_template('/userModule.html')
+    else:
+        render_template('/mainHome.html')
+
+@app.route('/newUSer', methods=['POST'])
+def newUser():
+    return addUser()
+
+
+@app.route('/',methods=['POST'] )               #will be mdified later
 def sendVal():
     complain = request.form['complain']
     putComplain(complain)
--- putComplain.py
+++ putComplain.py
@@ -1,24 +1,29 @@
+import mysql.connector, hashlib
+conn = mysql.connector.connect(
+    host = "localhost",
+    user ="root",
+    password = "",
+    database = "ProjectDB"
+)
+
 def putComplain(complainString):
-    import mysql.connector
-    conn = mysql.connector.connect(
-        host = "localhost",
-        user ="root",
-        password = "",
-        database = "ProjectDB"
-    )
     myCursor = conn.cursor()
     myCursor.execute("insert into complaints values(%s)",(complainString,))  #2nd param is a tuple
     conn.commit()
     conn.close()
+
+def verifyLogin(email, password):
+    hash_object = hashlib.md5(password.encode())
+    password = hash_object.hexdigest()
+    print("pass: ",password)
+    myCursor = conn.cursor()
+    myCursor.execute("select * from Users where email=%s AND pass=%s",(email,password))
+    
+    conn.commit()
+    conn.close()
+
 ########################
 # def putComplain(complainString):
-#     import mysql.connector
-#     conn = mysql.connector.connect(
-#         host = "localhost",
-#         user ="root",
-#         password = "",
-#         database = "ProjectDB"
-#     )
 #     myCursor = conn.cursor()
 #     myCursor.execute("select * from complaints")
 #     res = myCursor.fetchall()
