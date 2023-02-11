4851d4c870cd470d6d9cafb68727f92f4dc9d6c8
WinRAUL <rahul.chamoli.12@gmail.com>
2023-02-11 14:17:32 +0530

    updates!!

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from functionalities import encrypt, putComplain, verifyLogin, addUser
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session
 app = Flask(__name__)
 app.secret_key = "sessionKey"
 
@@ -12,37 +12,37 @@ def mainHome():
 def signup():
     return render_template('/signup.html')      
 
-@app.route('/login')                       #login.html == mainHome.html
-def login():
-    return render_template('/mainHome.html')
-
 @app.route('/verify', methods=['POST'])    #login verify and redirection
 def verify():
     if verifyLogin(request.form['logemail'], encrypt(request.form['pswd'])):
-        return render_template('/userModule.html')
+        return render_template('/userModule.html',email=request.form['logemail'])
     else:
-        flash("Invalid Credentials!")
+        flash("Invalid Credentials!","error")
         return render_template('/mainHome.html')
 
 @app.route('/newUser', methods=['POST'])   #adding user to DB
 def newUser():
     if addUser(request.form['email'], encrypt(request.form['pwd1']), encrypt(request.form['pwd2'])):
-        return render_template('/userModule.html')
+        return render_template('/userModule.html',email=request.form['email'])
     else:
         return render_template('/signup.html')
 
 @app.route('/userModule', methods=['POST'])   #adding complaint
 def newComplain():
-    dept='dept'
-    mail='test@mail.com'
-    putComplain(request.form['complain'],dept, mail)
-    return render_template('/userModule.html')
+    mail=request.form['email']
+    putComplain(request.form['complain'] ,mail)
+    return render_template('/userModule.html',email=mail)
 
 @app.route('/admin')
 def admin():
     return render_template('/adminModule.html')
 
-@app.errorhandler(404)
+@app.route('/logout')
+def logout():
+    flash("Logged Out","success")
+    return render_template('/mainHome.html')
+
+@app.errorhandler(404)      #to handle any non-specified endpoint
 def page_not_found(error):
     return render_template('404.html'), 404
 
--- functionalities.py
+++ functionalities.py
@@ -1,22 +1,23 @@
 import mysql.connector, hashlib
 from mysql.connector import Error
 from flask import flash
-conn = mysql.connector.connect(
+def connection():
+    return mysql.connector.connect(
     host = "localhost",
     user ="root",
     password = "",
     database = "ProjectDB"
 )
+conn = connection()
 myCursor = conn.cursor()
 
-
-def putComplain(complainString,dept,user):
+def putComplain(complainString,user):
     if(complainString==''):
-        flash("Empty request not acceptable")
+        flash("Empty response not acceptable","error")
         return
-    myCursor = conn.cursor()
     try:
-        myCursor.execute("insert into complaints(`Complains`, `Department`, `User`) values(%s,%s,%s)",(complainString,dept,user))  #2nd param is a tuple
+        myCursor = conn.cursor()
+        myCursor.execute("insert into complaints(`Complains`, `Department`, `User`) values(%s,%s,%s)",(complainString,'dept',user))  #2nd param is a tuple
         flash("Submitted succesfully","success")
         conn.commit()
     except Error as e:
@@ -28,25 +29,28 @@ def encrypt(password):
     return hash_object.hexdigest()
 
 def verifyLogin(email, password):
-    myCursor.execute("select * from users where Email=%s AND Pass=%s",(email,password))
-    res = myCursor.fetchall()
-    if(bool(res)):
-        return True
-    else:
+    try:
+        myCursor.execute("select * from users where Email=%s AND Pass=%s",(email,password))
+        res = myCursor.fetchall()
+        if(bool(res)):
+            return True
+        else:
+            return False
+    except Error as e:
+        flash("e","error")
         return False
 
 def addUser(email, pwd1, pwd2):
-        if pwd1!=pwd2:
-            flash("Password is not matching!")
+    if pwd1!=pwd2:
+        flash("Password is not matching!")
+        return False
+    else:
+        try:
+            myCursor.execute("insert into users(email,pass) values(%s,%s)",(email,pwd1))
+            conn.commit()
+            return True
+        except:
+            flash("Email alrerady in use")
             return False
-        else:
-            # if 1==2:
-            if (myCursor.execute("insert into users(email,pass) values(%s,%s)",(email,pwd1))):
-                conn.commit()
-                print("user added")
-                return True
-            else:
-                flash("Email alrerady in use")
-                return False
 
 # def getComplaints():
